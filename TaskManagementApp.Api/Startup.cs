3b48959f3f68617ef7148f446ea8ee8bbde3a032
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2020-10-09 02:15:10 +0300

    fluentvalidation methods are implemented

--- TaskManagementApp.Api/Startup.cs
+++ TaskManagementApp.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TaskManagementApp.Application;
+using TaskManagementApp.Infrastructure;
 
 namespace TaskManagementApp.Api
 {
@@ -25,6 +27,8 @@ namespace TaskManagementApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
+            services.AddInfrastructure();
             services.AddControllers();
         }
 
---
+++ TaskManagementApp.Application/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using TaskManagementApp.Application.Common.Behaviors;
+
+namespace TaskManagementApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+            return services;
+        }
+
+    }
+}
