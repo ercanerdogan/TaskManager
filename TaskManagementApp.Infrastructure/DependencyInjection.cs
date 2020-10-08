b74ce42d63427cb0e0f33edee3e4be9b8dcbdc38
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2020-10-09 01:34:21 +0300

    Repository pattern and unitOfWork pattern has been implemented

---
+++ TaskManagementApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagementApp.Application.Interfaces;
+using TaskManagementApp.Infrastructure.Repositories;
+
+namespace TaskManagementApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<ITaskRepository, TaskRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            return services;
+
+        }
+    }
+}
---
+++ TaskManagementApp.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagementApp.Application.Interfaces;
+
+namespace TaskManagementApp.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ITaskRepository Tasks { get; }
+        public UnitOfWork(ITaskRepository taskRepository)
+        {
+            Tasks = taskRepository;
+
+        }
+    }
+}
